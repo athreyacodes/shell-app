4c2933756d1dadd6eccdc5978096ae71ac6ba58f
athreyacodes <athreyamr10@gmail.com>
2025-01-24 03:34:42 +0530

    updating fonts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
 
+export const PORTFOLIO_DEV = "'https://portfolio-dev-5f375.firebaseapp.com";
+export const PORTFOLIO_ANGULAR =
+  "'https://portfolio-angular-d4f1f.firebaseapp.com";
+export const PORTFOLIO_AREACT = '';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
   {
@@ -8,8 +13,7 @@ export const routes: Routes = [
     loadComponent: () =>
       loadRemoteModule({
         type: 'module',
-        remoteEntry:
-          'https://portfolio-angular-d4f1f.firebaseapp.com/remoteEntry.js',
+        remoteEntry: PORTFOLIO_ANGULAR + '/remoteEntry.js',
         exposedModule: './Component',
       }).then((m) => m.AppComponent),
   },
