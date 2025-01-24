6daca7a00b05a33968a57dd92190eaf72c4dd2f7
athreyacodes <athreyamr10@gmail.com>
2025-01-24 15:50:38 +0530

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
 
-export const PORTFOLIO_DEV = "https://portfolio-dev-5f375.firebaseapp.com";
+export const PORTFOLIO_DEV = 'https://portfolio-dev-5f375.firebaseapp.com';
 export const PORTFOLIO_ANGULAR =
-  "https://portfolio-angular-d4f1f.firebaseapp.com";
+  'https://portfolio-angular-d4f1f.firebaseapp.com';
 export const PORTFOLIO_AREACT = '';
+
+export const PORTFOLIO_DEV_LOCAL = 'http://localhost:4202';
+export const PORTFOLIO_ANGULAR_LOCAL = 'http://localhost:4202/portfolio';
+export const PORTFOLIO_AREACT_LOCAL = '';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
@@ -13,7 +17,7 @@ export const routes: Routes = [
     loadComponent: () =>
       loadRemoteModule({
         type: 'module',
-        remoteEntry: PORTFOLIO_ANGULAR + '/remoteEntry.js',
+        remoteEntry: PORTFOLIO_ANGULAR_LOCAL + '/remoteEntry.js',
         exposedModule: './Component',
       }).then((m) => m.AppComponent),
   },
